157747a53f8268146e804f38bc97d3641b1c9d48
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-08-07 23:35:27 +0530

    Day 7

---
+++ 01_Arrays/Day 07/Q_13.cpp
@@ -0,0 +1,64 @@
+// Q. Kadane's Algorithm
+
+// https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        int curSum = 0;
+        int maxSum = INT_MIN;
+        
+        for(int i = 0; i < n; i++)
+        {
+            curSum = curSum + arr[i];
+            
+            if(curSum > maxSum)
+            {
+                maxSum = curSum;
+            }
+            if(curSum < 0)
+            {
+                curSum = 0;
+            }
+        }
+        
+        return maxSum;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+
+// } Driver Code Ends
