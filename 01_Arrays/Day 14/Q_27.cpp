4baeddeaab8a1b4b944091fbbaea3cb37906bacd
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-08-14 23:11:19 +0530

    Day 14

---
+++ 01_Arrays/Day 14/Q_27.cpp
@@ -0,0 +1,54 @@
+// Q. Array Subset of another array
+
+// https://practice.geeksforgeeks.org/problems/array-subset-of-another-array2317/1
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+string isSubset(int a1[], int a2[], int n, int m) ;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        int a1[n], a2[m];
+
+        for (int i = 0; i < n; i++) {
+            cin >> a1[i];
+        }
+        for (int i = 0; i < m; i++) {
+            cin >> a2[i];
+        }
+
+        cout << isSubset(a1, a2, n, m) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+string isSubset(int a1[], int a2[], int n, int m) {
+    set<int>s;
+   for(int i=0;i<n;i++)
+   {
+       s.insert(a1[i]);
+   }
+   int x=0;
+   for(int i=0;i<m;i++)
+   {
+       if(s.count(a2[i]))
+        {
+           x++;
+       }
+       if(x==m)
+       {
+           return "Yes";
+       }
+   }
+   return "No";
+}
---
+++ 01_Arrays/Day 14/Q_28.cpp
@@ -0,0 +1,4 @@
+// Q. Triplet Sum in Array
+
+// https://practice.geeksforgeeks.org/problems/triplet-sum-in-array-1587115621/1
+
