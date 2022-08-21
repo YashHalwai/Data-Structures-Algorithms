bacee9b311a4d0a5e75d17e2f520b5d9169227f4
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-08-21 23:52:50 +0530

    Day 21

---
+++ 01_Arrays/Day 21/Q_41.cpp
@@ -0,0 +1,56 @@
+// Q. Sorted matrix
+
+// https://practice.geeksforgeeks.org/problems/sorted-matrix2333/1
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> sortedMatrix(int N, vector<vector<int>> Mat) {
+        // code here
+            vector<int> temp;
+        for(int i=0; i<N; i++) {
+           for(int j=0; j<N; j++) {
+               temp.push_back(Mat[i][j]);
+           }
+       }
+       
+       sort(temp.begin(), temp.end());
+       
+       
+       for(int i=0; i<N*N; i++) {
+           Mat[i/N][i%N] = temp[i];
+       }
+       
+       return Mat;
+   
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> v(N, vector<int>(N));
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) cin >> v[i][j];
+        Solution ob;
+        v = ob.sortedMatrix(N, v);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) cout << v[i][j] << " ";
+            cout << "\n";
+        }
+    }
+}
+// } Driver Code Ends
---
+++ 01_Arrays/Day 21/Q_42.cpp
@@ -0,0 +1,4 @@
+// Q. Max rectangle
+
+// https://practice.geeksforgeeks.org/problems/max-rectangle/1
+
