54f675fd23a2cad6465fa8457d6a40b2151d2404
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-08-23 23:41:58 +0530

    Day 23

--- 01_Arrays/Day 23/Q_45.cpp
+++ 01_Arrays/Day 23/Q_45.cpp
@@ -2,3 +2,51 @@
 
 // https://practice.geeksforgeeks.org/problems/pairs-with-specific-difference1533/1
 
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int maxSumPairWithDifferenceLessThanK(int arr[], int N, int K)
+    {
+        // Your code goes here   
+        sort(arr, arr+N);
+        int sum = 0;
+        
+        for(int i=N-1; i>0; i--)
+        {
+            if(abs(arr[i] - arr[i-1]) < K)
+            {
+                sum += arr[i] + arr[i-1];
+                i--;
+            }
+        }
+        
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int N;
+		cin>>N;
+		int arr[N];
+		for(int i=0;i<N;i++)
+		cin>>arr[i];
+
+		int K;
+		cin>>K;
+        Solution ob;
+		cout<<ob.maxSumPairWithDifferenceLessThanK(arr,N,K)<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
---
+++ 01_Arrays/Day 23/Q_46.cpp
@@ -0,0 +1,4 @@
+// Q. Kth element in Matrix
+
+// https://practice.geeksforgeeks.org/problems/kth-element-in-matrix/1
+
