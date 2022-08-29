512af8ca3ff55325edd1fbec74e92ec5f75a8155
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-08-29 23:39:16 +0530

    Day 29

--- 03_Strings/Day 28/Q_56.cpp
+++ 03_Strings/Day 28/Q_56.cpp
@@ -2,3 +2,83 @@
 
 // https://practice.geeksforgeeks.org/problems/longest-palindrome-in-a-string3411/1
 
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string longestPalin (string S) {
+        // code here
+         int n = S.length();
+        int start = 0;
+        int len = 1;
+        
+        for(int i=0;i<n;i++)
+        {
+            // Even length substring by taking middle i
+            int left = i-1; 
+            int right = i+1;
+                // check untill left and right are equal thrn proceed else break;
+            while(left >=0 and right<= n-1)
+            {
+                if(S[left] == S[right])
+                {
+                    if(len < right-left+1)
+                    {
+                        len = right-left+1;
+                        start = left;    
+                    }
+                    left--;
+                    right++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            // odd length substring by taking middle i
+            left = i;
+            right = i+1;
+            while(left >=0 and right<= n-1)
+            {
+                if(S[left] == S[right])
+                {
+                    if(len < right-left+1)
+                    {
+                        len = right-left+1;
+                        start = left;    
+                    }
+                    left--;
+                    right++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+        }
+        return S.substr(start,len);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string S; cin >> S;
+        
+        Solution ob;
+        cout << ob.longestPalin (S) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
---
+++ 03_Strings/Day 29/Q_58.cpp
@@ -0,0 +1,4 @@
+// Q. String Subsequence Game
+
+// https://practice.geeksforgeeks.org/problems/string-subsequence-game5515/1
+
